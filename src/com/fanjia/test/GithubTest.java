6df33625f784c29aa28f305a83a3f1e16ad3dce3
fjdaima123 <1580332947@qq.com>
2020-09-28 15:04:28 +0800

    Initial commit 第二次熟悉github

---
+++ src/com/fanjia/test/GithubTest.java
@@ -0,0 +1,10 @@
+package com.fanjia.test;
+
+/**
+ * Created by FanJia on 2020/9/28 15:02
+ */
+public class GithubTest {
+    public static void main(String[] args) {
+        System.out.println("熟悉提交github中");
+    }
+}
